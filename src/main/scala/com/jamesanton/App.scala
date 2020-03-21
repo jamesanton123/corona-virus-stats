30552f422b43c1fb217cb7e0a1236567443c3168
james <jamesanton123@protonmail.com>
2020-03-20 22:15:15 -0400

    we have no friends

--- src/main/scala/com/jamesanton/App.scala
+++ src/main/scala/com/jamesanton/App.scala
@@ -2,33 +2,71 @@ package com.jamesanton
 
 object App extends App{
 
-    val downloader = new CSSEGISandDataDownloader
-    val populationService = new PopulationService
+  val downloader = new CSSEGISandDataDownloader
+  val populationService = new PopulationService
+  val countryNameMapper = Map(
+    "US" -> "United States",
+    "Italy" -> "Italy",
+    "Germany" -> "Germany",
+    "United Kingdom" -> "United Kingdom"
+  )
+  val stats = downloader.getStats(StatType.CONFIRMED)
 
-    val (countryStatDates, countryStats) = downloader.getStats(StatType.CONFIRMED)
-
-    val usStats = countryStats.filter(_.countryRegion.equalsIgnoreCase("us"))
-    val usStatsValues: Seq[Seq[Float]] = usStats.map(_.values)
-    val aggregateValuesUS = Array.fill(usStatsValues.head.length)(0f)
-    usStatsValues.foreach(regionValues=>{
-      for (i <- regionValues.indices) {
-        aggregateValuesUS(i) = aggregateValuesUS(i) + regionValues(i)
-      }
-    })
-
-    val countryNameMapper = Map(
-      "US" -> "United States"
-    )
-
-    val usPopulation = populationService.getPopulationForCountry(countryNameMapper.get("US").getOrElse(throw new IllegalArgumentException("Country name is not mapped")))
-
-    println(countryStatDates.zip(aggregateValuesUS).map{
-      case (date, value) =>
-        val perPopulationValue = value / usPopulation
-        val perPopulationAsAPercent = perPopulationValue * 100
-        f"As of $date, $perPopulationAsAPercent%.10f percent"
-    }.last)
+  // Filter the stats list for only countries in the mapper
+  val statsForCountriesInMapper = stats match {
+    case (dates, countryStats) =>
+      (dates, countryStats.filter(stat => countryNameMapper.contains(stat.countryRegion)))
   }
+
+  // Aggregate the us stats (because its broken up by states)
+  val groupedByCountryRegion = statsForCountriesInMapper match {
+    case (dates, countryStats) => {
+      val grouped = countryStats.groupBy(_.countryRegion)
+
+      def sum(regionStats: Seq[CountryStat], name: String): CountryStat = {
+        val aggregateValuesForCountry = Array.fill(dates.length)(0f)
+        regionStats.foreach(countryStat=>{
+          for (i <- aggregateValuesForCountry.indices) {
+            aggregateValuesForCountry(i) += countryStat.values(i)
+          }
+        })
+        CountryStat("", name, 0f, 0f, aggregateValuesForCountry)
+      }
+
+      val aggregates = grouped.map {
+        case (name, regionStats) => sum(regionStats, name)
+      }
+
+      (dates, aggregates)
+    }
+  }
+
+  // Filter the stats list for only countries in the mapper, and divides by population
+  val statsPerPopulation = groupedByCountryRegion match {
+    case (dates, countryStats) =>
+      (dates, countryStats.map {
+        countryStat => {
+          val population = getPopulationForCountry(countryStat.countryRegion)
+          countryStat.copy(values = countryStat.values.map(value => value / population))
+        }
+      })
+  }
+
+  private def getPopulationForCountry(name: String): Int = {
+    populationService.getPopulationForCountry(
+      countryNameMapper.get(name).getOrElse(throw new IllegalArgumentException("Country name is not mapped"))
+    )
+  }
+
+  statsPerPopulation match {
+    case(dates, countryStats) => countryStats.foreach(countryStat => {
+      val perPopulationAsAPercent = countryStat.values.last * 100
+      println(f"As of ${dates.last} in ${countryStat.countryRegion}, " +
+        f"$perPopulationAsAPercent%.10f percent, value was ${countryStat.values.last}")
+    })
+  }
+
+ }
 
 case class CountryStat(provinceState: String, countryRegion: String, lat: Float, long: Float, values: Seq[Float])
 
